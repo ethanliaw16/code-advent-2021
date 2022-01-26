27da7f7955b6db09dc352dcd1cc4f6e3d26b4b34
EthanLiaw <88673417+EthanLiaw@users.noreply.github.com>
2022-01-26 10:43:49 -0500

    refactored pathfinding, duplicate searches reduced

--- advent-day-15.py
+++ advent-day-15.py
@@ -1,21 +1,21 @@
 import sys
 
-def out_of_bounds(pair):
-    return pair[0] < 0 or pair[0] > 99 or pair[1] < 0 or pair[1] > 99
+def out_of_bounds(pair, num_rows, num_cols):
+    return pair[0] < 0 or pair[0] > (num_rows - 1) or pair[1] < 0 or pair[1] > (num_cols - 1)
 
-def get_adjacent(row, column):
+def get_adjacent(row, column, num_rows, num_cols):
     adjacent = []
-    top = [row - 1, column, (row - 1) * 100 + column]
-    left = [row, column - 1, (row * 100) + column - 1]
-    bottom = [row + 1, column, (row + 1) * 100 + column]
-    right = [row, column + 1, (row * 100) + column + 1]
-    if not out_of_bounds(top):
+    top = [row - 1, column, (row - 1) * num_cols + column]
+    left = [row, column - 1, (row * num_rows) + column - 1]
+    bottom = [row + 1, column, (row + 1) * num_cols + column]
+    right = [row, column + 1, (row * num_rows) + column + 1]
+    if not out_of_bounds(top, num_rows, num_cols):
         adjacent.append(top)
-    if not out_of_bounds(right):
+    if not out_of_bounds(right, num_rows, num_cols):
         adjacent.append(right)
-    if not out_of_bounds(left):
+    if not out_of_bounds(left, num_rows, num_cols):
         adjacent.append(left)
-    if not out_of_bounds(bottom):
+    if not out_of_bounds(bottom, num_rows, num_cols):
         adjacent.append(bottom)
     return adjacent
         
@@ -40,52 +40,36 @@ current_row = 0
 current_column = 0
 print(f"Map size: {num_rows} rows {num_cols} cols")
 
-while(current_row < destination_row and current_column < destination_column):
-    risk_of_right = board[current_row][current_column + 1]
-    risk_of_down = board[current_row + 1][current_column]
-    if(risk_of_down < risk_of_right):
-        path.append('D')
-        current_row += 1
-        total_risk += risk_of_down
-    else:
-        path.append('R')
-        current_column += 1
-        total_risk += risk_of_right
 
-print(f"we are now at {current_row} {current_column}")
+shortest_path_set = [111111111] * (num_rows * num_cols)
 
-if current_row < destination_row:
-    while(current_row < destination_row):
-        current_row += 1
-        total_risk += board[current_row][current_column]
-        path.append('D')
-
-if current_column < destination_column:
-    while(current_column < destination_column):
-        current_column += 1
-        total_risk += board[current_row][current_column]
-        path.append('R')
-
-shortest_path_set = [111111111] * 10000
-
-min_risks = [99999] * 10000
+min_risks = [99999] * (num_rows * num_cols)
 min_risks[0] = 0
 num_shortest_found = 0
-while shortest_path_set[9999] == 111111111:
+found = []
+while shortest_path_set[(num_rows * num_cols) - 1] == 111111111:
     position = min_risks.index(min(min_risks))
-    #print(f"shortest path set has {num_shortest_found}, sum of minimum risks is {sum(min_risks)}, cost to destination is {shortest_path_set[9999]}")
     shortest_path_set[position] = min_risks[position]
-    num_shortest_found += 1
+    #print(f"minimum in min_risks is at position {position}, with value of {min_risks[position]}")
+    #print(f"min_risks: {min_risks}")
+    if not position in found:
+        num_shortest_found += 1
+        if num_shortest_found % 1000 == 0:
+            print(f"{num_shortest_found} paths found, most recent is {position}")
+        found.append(position)
+    else:
+        print(f"uhhh this was already found")
+        
     position_row = int((position - position % num_rows) / num_rows)
     position_col = position % num_rows
-    adjacent = get_adjacent(position_row, position_col)
+    adjacent = get_adjacent(position_row, position_col, num_rows, num_cols)
     for pair in adjacent:
         row = pair[0]
         column = pair[1]
         vector_position = pair[2]
-        if min_risks[position] + board[row][column] < min_risks[vector_position]:
+        if min_risks[position] + board[row][column] < min_risks[vector_position] and not vector_position in found:
             min_risks[vector_position] = min_risks[position] + board[row][column]
-    min_risks[position] = 9999999
+    min_risks[position] = 99999
 
-print(f"The total risk is {shortest_path_set[9999]}") #621
+print(f"The total risk is {shortest_path_set[(num_rows * num_cols) - 1]}") #621
 #print(f"the path is {path}")
