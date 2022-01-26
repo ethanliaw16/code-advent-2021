037c61211989a37acc356b8644fab2021ede2445
EthanLiaw <88673417+EthanLiaw@users.noreply.github.com>
2022-01-26 12:49:44 -0500

    finished day 15 part 2, very slow

--- advent-day-15.py
+++ advent-day-15.py
@@ -34,20 +34,51 @@ path = []
 total_risk = 0
 num_rows = len(board)
 num_cols = len(board[0])
+
+#board augmentation
+full_board = []
+for i in range(5 * num_rows):
+    full_board.append([0] * 5 * num_cols)
+
+for i in range(num_rows):
+    for j in range(num_cols):
+        #print(f"updating board {i} {j}. on the original this is {board[i][j]}")
+        full_board[i][j] = board[i][j]
+        
+
+
+
+for i in range(5):
+    for j in range(5):
+        for row in range(num_rows):
+            for col in range(num_cols):
+                new_val = board[row][col] + i + j
+                if new_val > 9:
+                    new_val -= 9
+                full_board[row + (num_rows * i)][col + (num_cols * j)] = new_val
+
+#print("\n")
+#for row in full_board:
+#    print(row)
+#print("\n")    
+final_destination_row = (5 * num_rows) - 1
+final_destination_column = (5 * num_cols) - 1
+
 destination_row = num_rows - 1
 destination_column = num_cols - 1
 current_row = 0
 current_column = 0
 print(f"Map size: {num_rows} rows {num_cols} cols")
+print(f"Final map size: {len(full_board)} rows {len(full_board[0])}")
 
+shortest_path_set = [111111111] * (num_rows * num_cols * 25)
 
-shortest_path_set = [111111111] * (num_rows * num_cols)
-
-min_risks = [99999] * (num_rows * num_cols)
+min_risks = [9999999] * (num_rows * num_cols * 25)
 min_risks[0] = 0
 num_shortest_found = 0
 found = []
-while shortest_path_set[(num_rows * num_cols) - 1] == 111111111:
+
+while shortest_path_set[(num_rows * num_cols * 25) - 1] == 111111111:
     position = min_risks.index(min(min_risks))
     shortest_path_set[position] = min_risks[position]
     #print(f"minimum in min_risks is at position {position}, with value of {min_risks[position]}")
@@ -58,18 +89,20 @@ while shortest_path_set[(num_rows * num_cols) - 1] == 111111111:
             print(f"{num_shortest_found} paths found, most recent is {position}")
         found.append(position)
     else:
-        print(f"uhhh this was already found")
+        print(f"uhhh this was already found. length of shortest path set: {len(shortest_path_set)}. length of min_risks: {len(min_risks)}")
+        print(f"{shortest_path_set[2499]} {min_risks[2499]}. min: {min(min_risks)}")
         
-    position_row = int((position - position % num_rows) / num_rows)
-    position_col = position % num_rows
-    adjacent = get_adjacent(position_row, position_col, num_rows, num_cols)
+    position_row = int((position - position % (num_rows * 5)) / (num_rows * 5))
+    position_col = position % (num_rows * 5)
+    adjacent = get_adjacent(position_row, position_col, num_rows * 5, num_cols * 5)
     for pair in adjacent:
         row = pair[0]
         column = pair[1]
         vector_position = pair[2]
-        if min_risks[position] + board[row][column] < min_risks[vector_position] and not vector_position in found:
-            min_risks[vector_position] = min_risks[position] + board[row][column]
-    min_risks[position] = 99999
+        if min_risks[position] + full_board[row][column] < min_risks[vector_position] and not vector_position in found:
+            min_risks[vector_position] = min_risks[position] + full_board[row][column]
+            
+    min_risks[position] = 9999999
 
-print(f"The total risk is {shortest_path_set[(num_rows * num_cols) - 1]}") #621
+print(f"The total risk is {shortest_path_set[(num_rows * num_cols * 25) - 1]}") #621
 #print(f"the path is {path}")
